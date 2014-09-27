05b578b9a0658a4c8c7f07dca6348c678cf9755d
philhofer <phofer@umich.edu>
2014-09-27 17:56:51 -0400

    minimal single-connection implementation

---
+++ client.go
@@ -0,0 +1,68 @@
+package synapse
+
+import (
+	"errors"
+	"github.com/philhofer/msgp/enc"
+	"io"
+	"net/rpc"
+)
+
+var (
+	badParams  = errors.New("bad parameters")
+	notPending = errors.New("not pending")
+)
+
+// clientCodec is the single-connection
+// implementation of rpc.ClientCodec
+type clientCodec struct {
+	conn io.ReadWriteCloser
+}
+
+func (c *clientCodec) Close() error {
+	return c.conn.Close()
+}
+
+func (c *clientCodec) WriteRequest(r *rpc.Request, param interface{}) error {
+	wr, ok := param.(io.WriterTo)
+	if !ok {
+		return badParams
+	}
+	return writeReq(c.conn, r, wr)
+}
+
+func (c *clientCodec) ReadResponseHeader(r *rpc.Response) error {
+	return readRes(c.conn, r)
+}
+
+func (c *clientCodec) ReadResponseBody(x interface{}) error {
+	rf, ok := x.(io.ReaderFrom)
+	if !ok {
+		return badParams
+	}
+	_, err := rf.ReadFrom(c.conn)
+	return err
+}
+
+func writeReq(w io.Writer, r *rpc.Request, wrt io.WriterTo) (err error) {
+	en := enc.NewEncoder(w)
+	_, err = en.WriteString(r.ServiceMethod)
+	if err != nil {
+		return
+	}
+	_, err = en.WriteUint64(r.Seq)
+	if err != nil {
+		return
+	}
+	_, err = en.WriteIdent(wrt)
+	return
+}
+
+func readRes(rd io.Reader, r *rpc.Response) (err error) {
+	dc := enc.NewDecoder(rd)
+	r.ServiceMethod, _, err = dc.ReadString()
+	if err != nil {
+		return
+	}
+	r.Seq, _, err = dc.ReadUint64()
+	return
+}
---
+++ server.go
@@ -0,0 +1,52 @@
+package synapse
+
+import (
+	"github.com/philhofer/msgp/enc"
+	"io"
+	"net/rpc"
+)
+
+type serverCodec struct {
+	conn io.ReadWriteCloser
+}
+
+func (s *serverCodec) Close() error {
+	return s.conn.Close()
+}
+
+func (s *serverCodec) ReadRequestHeader(r *rpc.Request) error {
+	return readReq(s.conn, r)
+}
+
+func (s *serverCodec) ReadRequestBody(x interface{}) error {
+	rd, ok := x.(io.ReaderFrom)
+	if !ok {
+		return badParams
+	}
+	_, err := rd.ReadFrom(s.conn)
+	return err
+}
+
+func readReq(r io.Reader, req *rpc.Request) (err error) {
+	dc := enc.NewDecoder(r)
+	req.ServiceMethod, _, err = dc.ReadString()
+	if err != nil {
+		return
+	}
+	req.Seq, _, err = dc.ReadUint64()
+	return
+}
+
+func writeRes(w io.Writer, hdr *rpc.Response, body io.WriterTo) (err error) {
+	en := enc.NewEncoder(w)
+	_, err = en.WriteString(hdr.ServiceMethod)
+	if err != nil {
+		return
+	}
+	_, err = en.WriteUint64(hdr.ServiceMethod)
+	if err != nil {
+		return
+	}
+	_, err = en.WriteIdent(body)
+	return
+}

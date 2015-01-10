75ba8cec0513d7253bc1eee71c79c4a8127077c5
philhofer <phofer@umich.edu>
2015-01-10 17:18:33 -0500

    re-generate files w/o extra methods

--- _examples/arith/server/def_gen_test.go
+++ _examples/arith/server/def_gen_test.go
@@ -6,65 +6,10 @@ package main
 
 import (
 	"testing"
-	"bytes"
+
 	"github.com/philhofer/msgp/msgp"
 )
 
-
-func TestNumEncodeDecode(t *testing.T) {
-	v := new(Num)
-	var buf bytes.Buffer
-	msgp.Encode(&buf, v)
-
-	m := v.Msgsize()
-	if buf.Len() > m {
-		t.Logf("WARNING: Maxsize() for %v is inaccurate", v)
-	}
-
-	vn := new(Num)
-	err := msgp.Decode(&buf, vn)
-	if err != nil {
-		t.Error(err)
-	}
-
-	buf.Reset()
-	msgp.Encode(&buf, v)
-	err = msgp.NewReader(&buf).Skip()
-	if err != nil {
-		t.Error(err)
-	}
-}
-
-func BenchmarkNumEncode(b *testing.B) {
-	v := new(Num)
-	var buf bytes.Buffer
-	msgp.Encode(&buf, v)
-	b.SetBytes(int64(buf.Len()))
-	en := msgp.NewWriter(msgp.Nowhere)
-	b.ReportAllocs()
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		v.EncodeMsg(en)
-	}
-	en.Flush()
-}
-
-func BenchmarkNumDecode(b *testing.B) {
-	v := new(Num)
-	var buf bytes.Buffer
-	msgp.Encode(&buf, v)
-	b.SetBytes(int64(buf.Len()))
-	rd := msgp.NewEndlessReader(buf.Bytes())
-	dc := msgp.NewReader(rd)
-	b.ReportAllocs()
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		err := v.DecodeMsg(dc)
-		if err != nil {
-			b.Fatal(err)
-		}
-	}
-}
 func TestNumMarshalUnmarshal(t *testing.T) {
 	v := new(Num)
 	bts, err := v.MarshalMsg(nil)

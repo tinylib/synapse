c10685a6de278926c2016af9653911671e843620
philhofer <phofer@umich.edu>
2015-01-17 15:10:31 -0500

    update example import

--- _examples/arith/server/main.go
+++ _examples/arith/server/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/philhofer/synapse"
+	"github.com/tinylib/synapse"
 	"net"
 )
 
--- _examples/hello_world/server/main.go
+++ _examples/hello_world/server/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/philhofer/synapse"
+	"github.com/tinylib/synapse"
 	"log"
 )
 
